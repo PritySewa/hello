454ac43da5582502dbc0301fca64fb09f50c295f
PritySewa <bangatng7@gmail.com>
2024-12-10 15:15:51 +0545

    Initial Commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use \Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+    public function getwelcome()
+    {
+        return view("welcome");
+
+    }git
+
+
+    public function getMypage()
+    {
+        return view(view: "test.home");
+    }
+
+
+
+
+
+
+
+}
